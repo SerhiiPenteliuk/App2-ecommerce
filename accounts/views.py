09e2b681c20e906b590cc4246e22d8c1a65859e9
SerhiiPenteliuk <sergunyapw@gmail.com>
2022-02-10 12:06:52 +0200

    timeoutError

--- accounts/views.py
+++ accounts/views.py
@@ -39,8 +39,8 @@ def register(request):
             to_email = email
             send_email = EmailMessage(mail_subject, message, to=[to_email])
             send_email.send()
-            messages.success(request, 'Registration successful.')
-            return redirect('register')
+            # messages.success(request, 'Email verification is sent to your email address')
+            return redirect('/accounts/login/?command=verification&email='+email)
     else:
         form = RegistrationForm()
     context = {
@@ -73,5 +73,18 @@ def logout(request):
     return redirect('login')
 
 
-def activate(request):
-    return
+def activate(request, uidb64, token):
+    try:
+        uid = urlsafe_base64_decode(uidb64).decode()
+        user = Account._default_manager.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, Account.DoesNotExist):
+        user = None
+
+    if user is not None and default_token_generator.check_token(user, token):
+        user.is_active = True
+        user.save()
+        messages.success(request, 'Congratulations! Your account is activated!')
+        return redirect('login')
+    else:
+        messages.error(request, 'Invalid activation link')
+        return redirect('register')
